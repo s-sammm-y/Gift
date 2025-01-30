51c7109b7a74a592a626d424403b26e94f683c55
Sourasish Biswas <sourasishbiswas7@gmail.com>
2025-01-31 00:23:10 +0530

    changed again

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,10 @@ function App() {
   const [buttonText,setButtonText] = useState("TAP HERE BBG");
   const timeoutRef = useRef(null);
   const [isTypingComplete,setIsTypingComplete] = useState(false);
-  const fullText="Hey,Ana thank you for waiting all this moment for the gifts.I wnated to let you know that I am very grateful for the fact that I got the love of a person so beautiful and astonoshing like you,I mean its funny how an average guy like me can be loved like the way you do.I bet you can understand what kind of person I am,filled with trust issues,and you act accordingly handeling my sensitive mind.I deeply appreciate your patience and things you do for me.Never though I would love someone so much,I though I cannot be loved because of the way I am.I made this website for you it may not be like the professionals but the truth is every line of code consits of my love and trust.Ill make more fun softwares in the future but for now I just wanted give you a small taste of how I can appreciate you in funny ways.But as I said this website is not that impressive in comaprison to the softwares I made,I wanted to try out a new love language I hope you will like it and as always I Love You Ana My life wont have any reason without you,and I cannot imagine a world without your presence in it."
+  const fullText="f."
   const audioRef = useRef(null);
   const [sendMessegeTextDisplay,setSendMessageTextDisplay] = useState('');
   const [checkResponse,setCheckResponse]=useState("");
-  const [showButton,setShowButton] = useState(true);
   
   const typewriter = (textToType, i = 0) => {
     if (i < textToType.length) {
@@ -79,11 +78,12 @@ function App() {
       console.log(response.data.message);
       setCheckResponse('')
       setSendMessageTextDisplay('Check your phone Message Inbox babygirl');
-      setShowButton(false);
+      setIsTypingComplete(false)
 
     }catch(err){
       console.error('Error making post request',err);
       setCheckResponse('Something is wrong tell your Boyfriend to fix me');
+      setIsTypingComplete(false);
     }
   }
 
@@ -112,7 +112,7 @@ function App() {
           </section>
 
           <div className='Ending p-4'>
-            {isTypingComplete && showButton && (
+            {isTypingComplete && (
             <button className='h-15 last' 
             onClick={handleSendMessage}>
               <p className='pacifico-regular text-sm'>
